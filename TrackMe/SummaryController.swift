f5a84ab8f563d673fc693bb28f28d7a5b9d2bbd2
HBJ <haoboji@gmail.com>
2016-06-09 16:08:31 +1000

    all charts

--- TrackMe/SummaryController.swift
+++ TrackMe/SummaryController.swift
@@ -21,7 +21,6 @@ class SummaryController: UIViewController, ChartViewDelegate {
     let healthStore = HKHealthStore()
     var managedObjectContext: NSManagedObjectContext
     let weekdays: [String] = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
-    let travelingDistances: [Double] = [1, 2, 3, 4, 5, 6, 7]
     var currentWeekStartDay: NSDate?
     var thisWeekStartDay: NSDate?
 
@@ -47,20 +46,7 @@ class SummaryController: UIViewController, ChartViewDelegate {
         self.navigationItem.title = "This Week"
         thisWeekStartDay = calculateWeekStartDay(NSDate())
         currentWeekStartDay = calculateWeekStartDay(NSDate())
-
-        // Traveling chart
-        let weeklyLocations = fetchWeeklyLocations(thisWeekStartDay!)
-        let weeklyTravelingDistances = calculateWeeklyTravelingDistances(weeklyLocations)
-        print(weeklyTravelingDistances)
-        setTravelingDistanceChartData(weeklyTravelingDistances)
-
-        // Walking chart
-        checkAvailability()
-        let type = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
-        fetchWeeklyHealthData(thisWeekStartDay!, type: type!, unit: HKUnit.meterUnit()) { weeklyHealthData in
-            print(weeklyHealthData)
-        }
-
+        updateCharts()
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,7 +57,7 @@ class SummaryController: UIViewController, ChartViewDelegate {
     @IBAction func prevWeek(sender: UIBarButtonItem) {
         currentWeekStartDay = currentWeekStartDay!.dateByAddingTimeInterval(-24 * 60 * 60 * 7)
         printTitle()
-        updateTravelingDistancesChart()
+        updateCharts()
     }
 
     @IBAction func nextWeek(sender: UIBarButtonItem) {
@@ -80,7 +66,7 @@ class SummaryController: UIViewController, ChartViewDelegate {
         }
         currentWeekStartDay = currentWeekStartDay!.dateByAddingTimeInterval(24 * 60 * 60 * 7)
         printTitle()
-        updateTravelingDistancesChart()
+        updateCharts()
     }
 
     func checkAvailability() -> Bool {
@@ -109,10 +95,28 @@ class SummaryController: UIViewController, ChartViewDelegate {
         }
     }
 
-    func updateTravelingDistancesChart() {
+    func updateCharts() {
+        // Traveling chart
         let weeklyLocations = fetchWeeklyLocations(currentWeekStartDay!)
         let weeklyTravelingDistances = calculateWeeklyTravelingDistances(weeklyLocations)
         setTravelingDistanceChartData(weeklyTravelingDistances)
+        travelingDistanceChart.animate(yAxisDuration: 1)
+        // Walking chart
+        let typeWalk = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning)
+        fetchWeeklyHealthData(currentWeekStartDay!, type: typeWalk!, unit: HKUnit.meterUnit()) { weeklyHealthData in
+            dispatch_async(dispatch_get_main_queue(), {
+                self.setWalkingDistanceChartData(weeklyHealthData)
+                self.walkingDistanceChart.animate(yAxisDuration: 1)
+            })
+        }
+        // Steps chart
+        let typeSteps = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+        fetchWeeklyHealthData(currentWeekStartDay!, type: typeSteps!, unit: HKUnit.countUnit()) { weeklyHealthData in
+            dispatch_async(dispatch_get_main_queue(), {
+                self.setStepsChartData(weeklyHealthData)
+                self.stepsCountChart.animate(yAxisDuration: 1)
+            })
+        }
     }
 
     func calculateWeeklyTravelingDistances(weeklyLocations: [NSArray]) -> [Double] {
@@ -195,21 +199,39 @@ class SummaryController: UIViewController, ChartViewDelegate {
         travelingDistanceChart.descriptionText = ""
         travelingDistanceChart.legend.enabled = false
         travelingDistanceChart.rightAxis.drawLabelsEnabled = false
+        travelingDistanceChart.rightAxis.drawAxisLineEnabled = false
         travelingDistanceChart.rightAxis.axisMinValue = 0
         travelingDistanceChart.leftAxis.axisMinValue = 0
         travelingDistanceChart.xAxis.labelPosition = .Bottom
         travelingDistanceChart.xAxis.drawGridLinesEnabled = false
         travelingDistanceChart.xAxis.setLabelsToSkip(0)
+        travelingDistanceChart.userInteractionEnabled = false
+
         // Walking chart
         walkingDistanceChart.noDataText = "No Walking Data Provided."
         walkingDistanceChart.descriptionText = ""
         walkingDistanceChart.legend.enabled = false
         walkingDistanceChart.rightAxis.drawLabelsEnabled = false
+        walkingDistanceChart.rightAxis.drawAxisLineEnabled = false
         walkingDistanceChart.rightAxis.axisMinValue = 0
         walkingDistanceChart.leftAxis.axisMinValue = 0
         walkingDistanceChart.xAxis.labelPosition = .Bottom
         walkingDistanceChart.xAxis.drawGridLinesEnabled = false
         walkingDistanceChart.xAxis.setLabelsToSkip(0)
+        walkingDistanceChart.userInteractionEnabled = false
+
+        // Steps chart
+        stepsCountChart.noDataText = "No Steps Data Provided."
+        stepsCountChart.descriptionText = ""
+        stepsCountChart.legend.enabled = false
+        stepsCountChart.rightAxis.drawLabelsEnabled = false
+        stepsCountChart.rightAxis.drawAxisLineEnabled = false
+        stepsCountChart.rightAxis.axisMinValue = 0
+        stepsCountChart.leftAxis.axisMinValue = 0
+        stepsCountChart.xAxis.labelPosition = .Bottom
+        stepsCountChart.xAxis.drawGridLinesEnabled = false
+        stepsCountChart.xAxis.setLabelsToSkip(0)
+        stepsCountChart.userInteractionEnabled = false
     }
 
     func setTravelingDistanceChartData(travelingDistances: [Double]) {
@@ -234,5 +256,45 @@ class SummaryController: UIViewController, ChartViewDelegate {
         dataSets.append(set1)
         let data: LineChartData = LineChartData(xVals: weekdays, dataSets: dataSets)
         self.travelingDistanceChart.data = data
+    }
+
+    func setWalkingDistanceChartData(walkingDistances: [Double]) {
+        var yVals: [ChartDataEntry] = [ChartDataEntry]()
+        for i in 0 ..< weekdays.count {
+            yVals.append(ChartDataEntry(value: walkingDistances[i], xIndex: i))
+        }
+        let set1: LineChartDataSet = LineChartDataSet(yVals: yVals, label: "First Set")
+        set1.axisDependency = .Left
+        set1.setColor(UIColor.greenColor().colorWithAlphaComponent(0.5))
+        set1.setCircleColor(UIColor.greenColor())
+        set1.lineWidth = 2.0
+        set1.circleRadius = 4.0
+        set1.fillAlpha = 65 / 255.0
+        set1.fillColor = UIColor.greenColor()
+        set1.drawFilledEnabled = true
+        set1.highlightColor = UIColor.whiteColor()
+        set1.drawCircleHoleEnabled = true
+        set1.mode = .CubicBezier
+        set1.cubicIntensity = 0.2
+        var dataSets: [LineChartDataSet] = [LineChartDataSet]()
+        dataSets.append(set1)
+        let data: LineChartData = LineChartData(xVals: weekdays, dataSets: dataSets)
+        self.walkingDistanceChart.data = data
+    }
+
+    func setStepsChartData(steps: [Double]) {
+        print(steps)
+        var yVals: [BarChartDataEntry] = [BarChartDataEntry]()
+        for i in 0 ..< weekdays.count {
+            yVals.append(BarChartDataEntry(value: steps[i], xIndex: i))
+        }
+        let set1: BarChartDataSet = BarChartDataSet(yVals: yVals, label: "First Set")
+        set1.axisDependency = .Left
+        set1.setColor(UIColor.blueColor().colorWithAlphaComponent(0.5))
+
+        var dataSets: [BarChartDataSet] = [BarChartDataSet]()
+        dataSets.append(set1)
+        let data: BarChartData = BarChartData(xVals: weekdays, dataSets: dataSets)
+        self.stepsCountChart.data = data
     }
 }
